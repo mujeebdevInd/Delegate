8112bbe436d1591bc1ff14f2343b8f32c6a6686b
mujeebfive <mujeebfive@gmail.com>
2024-07-16 00:04:17 +0530

    Add project files.

---
+++ AnonymousDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AnonymousDelegates
+{
+    //Q. What are Anonymous Delegates in C#?
+    //In Anonymous Delegates, You can create a delegate,
+    //but there is no need to declare the method associated with it.
+    public delegate void Calculator(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calcAdd = delegate (int a, int b)
+            {
+                //Inline content of the method;
+                Console.WriteLine(a + b);
+            };
+
+            calcAdd(10, 20);
+        }
+    }
+
+    //Ouput: 30
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Delegates
+{
+    //Q. What you mean by Delegate? When to use them?
+    //A Delegate is a variable that holds the REFERENCE TO A METHOD. Or you can say it’s a pointer to a function.
+    //A delegate can refer to more than one methods of same return type and parameters.
+    //A Normal Delegate
+    delegate void Calculator(int x, int y);
+    class Program
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static void Main(string[] args)
+        {
+            //Instantiating Delegate
+            Calculator calc = new Calculator(Add);
+
+            //Calling method using delegate
+            calc(20, 40);
+
+            //Output: 60
+        }
+
+    }
+}
